ed61e1aa9a99bc2ce4d980bf58fedfc7966d7589
Ubuntu <ubuntu@ip-172-31-45-158.ec2.internal>
2026-07-20 06:32:19 +0000

    update config and s3 integration

--- reachx-frontend/vite.config.js
+++ reachx-frontend/vite.config.js
@@ -3,8 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { 
+  server: {
+  host: "0.0.0.0",
+  port: 3000,
+  allowedHosts: true,
+},
+  preview: {
     port: 3000,
     host: "0.0.0.0",
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "gtmreach.proplusdata.co",
+      "www.gtmreach.proplusdata.co",
+    ],
   },
 });
+
